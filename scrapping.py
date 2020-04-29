9149ba95f58fee8bd98a9c44a44c449f1f7caa3f
MargeshPatel01 <55601451+MargeshPatel01@users.noreply.github.com>
2020-04-29 16:23:56 -0400

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 options_headless = Options()
 options_headless.add_argument('--headless')
 options_headless.add_argument('--disable-gpu')  # Last I checked this was necessary.
-driver = webdriver.Chrome(executable_path=r'https://github.com/MargeshPatel01/flutter_icare_chatbot/blob/master/chromedriver.exe', options = options_headless  )
+driver = webdriver.Chrome()
     
 
 def get_url(postal_code):
