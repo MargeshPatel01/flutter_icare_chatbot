f6dbcbc05d20a5ee710c0aec43e549c221aeba49
MargeshPatel01 <55601451+MargeshPatel01@users.noreply.github.com>
2020-04-30 15:24:23 -0400

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -18,7 +18,7 @@ options_headless.add_argument('--disable-gpu')
 options_headless.add_argument('--headless')
   # Last I checked this was necessary.
 # chrome_options.add_argument('--no-sandbox')
-browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, option_headless=chrome_options)    
+browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=option_headless)    
 
 def get_url(postal_code):
 
